b8c10a5ea1b38d9f072c057da9ffd27c9338e3ae
DPHamdev <dphamill@gmail.com>
2022-01-13 21:22:10 +0000

    completed homepage header and CSS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -99,7 +99,7 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
-
+ 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -148,6 +148,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = "/media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
